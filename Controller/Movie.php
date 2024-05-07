3ca17b4c2d1c8d8cf2fd5527459979739436c7ea
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-05-07 18:03:35 +0200

    foreach index

--- Controller/Movie.php
+++ Controller/Movie.php
@@ -1,12 +1,15 @@
 <?php 
 include_once __DIR__. "/../Models/Movie.php";
+$movies = [];
  foreach ($products as $type => $value) {
-    echo $type; //movies e books
+   /*  echo $type;  *///movies e books
     if($type == 'books') {
        
         foreach( $value as $item ) {
            $movie = new Movie($item['title']);
-            var_dump($item); 
-            echo $item['title'];
+           $movies[] = $movie;
+           /*  var_dump($item); 
+            echo $item['title']; */
           }
     }}  
+var_dump($movies);
--- index.php
+++ index.php
@@ -1,20 +1,35 @@
-<?php 
+<?php
 //includi controller
 include_once __DIR__ . '/Controller/Movie.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH' crossorigin='anonymous'>
 </head>
-<body>
-<?php 
-   
 
-?>
-    
+<body>
+    <div class= "container">
+    <div class="row">
+        <?php foreach($movies as $movie) { ?>
+        <div class="card w-25 ">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $movie -> title ?></h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <?php } ?>
+    </div>
+    </div>
+
 </body>
+
 </html>
